7a485a089bb13166d7ff41803fc9a20e795be6ee
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-07-30 22:22:50 +0300

    initial commit

---
+++ src/main/java/com/otagrove/anomaly_finder_backend/AnomalyFinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.otagrove.anomaly_finder_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalyFinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomalyFinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otagrove/anomaly_finder_backend/AnomalyFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otagrove.anomaly_finder_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnomalyFinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
