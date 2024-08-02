54a07207d3550a3060bb0968db59e581f3261551
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-02 13:49:21 +0300

    add version controller

---
+++ src/main/java/com/otagrove/anomaly_finder_backend/controllers/VersionController.java
@@ -0,0 +1,13 @@
+package com.otagrove.anomaly_finder_backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    @GetMapping("/version")
+    public String version() {
+        return "0.0.1";
+    }
+}
