4ac3dd2d6b96d90b98a9359a1f644353c8071a2b
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-16 19:41:16 +0300

    add NoArgsConstructor to Score

--- src/main/java/com/ryazangrove/anomaly_finder_backend/models/Score.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/models/Score.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Score {
 
     @Id
