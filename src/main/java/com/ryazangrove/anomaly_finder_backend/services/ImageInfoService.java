ff2244dbf2dc393e59ba3ff7b61e70e66801de02
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-22 21:36:52 +0300

    add getImageInfo for ImageInfoService

---
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/services/ImageInfoService.java
@@ -0,0 +1,19 @@
+package com.ryazangrove.anomaly_finder_backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ryazangrove.anomaly_finder_backend.exceptions.ImageInfoNotFoundException;
+import com.ryazangrove.anomaly_finder_backend.models.ImageInfo;
+import com.ryazangrove.anomaly_finder_backend.repository.ImageInfoRepository;
+
+@Service
+public class ImageInfoService {
+    
+    @Autowired
+    private ImageInfoRepository imageInfoRepository;
+
+    public ImageInfo getImageInfo(Long id) throws ImageInfoNotFoundException {
+        return imageInfoRepository.findById(id).orElseThrow(() -> new ImageInfoNotFoundException(id));
+    }
+}
