6dd92dfb52e8e1f99e1052b973c3c7c28551a66e
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-22 23:46:00 +0300

    add image info endpoint to ImageController

--- src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/ImageController.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/ImageController.java
@@ -4,7 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ryazangrove.anomaly_finder_backend.exceptions.ImageInfoNotFoundException;
+import com.ryazangrove.anomaly_finder_backend.services.ImageInfoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +23,9 @@ import java.io.InputStream;
 @RequestMapping("/image")
 public class ImageController {
 
+    @Autowired
+    private ImageInfoService imageInfoService;
+
     @GetMapping("/{id}")
     public void serveImageHandler(@PathVariable Long id, HttpServletResponse response){
         try { 
@@ -26,5 +36,14 @@ public class ImageController {
             e.printStackTrace(); 
             response.setStatus(404);
         } 
+    }
+
+    @GetMapping("/info/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(imageInfoService.getImageInfo(id));
+        } catch (ImageInfoNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
     }
 }
