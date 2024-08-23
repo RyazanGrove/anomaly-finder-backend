ea7f98e4781822c8f0b4464e17add1f7031e7448
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-23 19:33:05 +0300

    add string constructor for ImageInfoNotFoundException to display fileName

--- src/main/java/com/ryazangrove/anomaly_finder_backend/exceptions/ImageInfoNotFoundException.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/exceptions/ImageInfoNotFoundException.java
@@ -4,4 +4,8 @@ public class ImageInfoNotFoundException extends RuntimeException {
     public ImageInfoNotFoundException(Long id) {
         super("Could not find metadata for image with id " + id);
     }
+
+    public ImageInfoNotFoundException(String fileName) {
+        super("Could not find metadata for image with file name " + fileName);
+    }
 }
