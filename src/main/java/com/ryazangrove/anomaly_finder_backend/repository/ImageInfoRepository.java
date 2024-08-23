238e366fa117190555eba26b36045534b99c3002
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-23 19:34:17 +0300

    add search query for ImageInfo by fileName

--- src/main/java/com/ryazangrove/anomaly_finder_backend/repository/ImageInfoRepository.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/repository/ImageInfoRepository.java
@@ -1,8 +1,11 @@
 package com.ryazangrove.anomaly_finder_backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ryazangrove.anomaly_finder_backend.models.ImageInfo;
 
 public interface ImageInfoRepository extends JpaRepository<ImageInfo, Long> {
+    Optional<ImageInfo> findByFileName(String fileName);
 }
