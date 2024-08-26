d6fef2571cb1a9e5823e49eabd39e6b4a9ae6a2d
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-26 17:29:30 +0300

    add query to getRandomImageInfos with specified amount

--- src/main/java/com/ryazangrove/anomaly_finder_backend/repository/ImageInfoRepository.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/repository/ImageInfoRepository.java
@@ -1,11 +1,16 @@
 package com.ryazangrove.anomaly_finder_backend.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ryazangrove.anomaly_finder_backend.models.ImageInfo;
 
 public interface ImageInfoRepository extends JpaRepository<ImageInfo, Long> {
     Optional<ImageInfo> findByFileName(String fileName);
+
+    @Query(value = "SELECT * FROM IMAGE_INFO ORDER BY RAND() LIMIT ?1", nativeQuery = true)
+    List<ImageInfo> getRandomImageInfos(int num);
 }
