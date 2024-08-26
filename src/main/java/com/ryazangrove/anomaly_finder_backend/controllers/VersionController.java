cb7f80352076642bdcb52390f193c7e473a86eed
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-26 16:57:11 +0300

    update version controller to be consistent

--- src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/VersionController.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/VersionController.java
@@ -1,5 +1,6 @@
 package com.ryazangrove.anomaly_finder_backend.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class VersionController {
 
     @GetMapping("/version")
-    public String version() {
-        return "0.0.3";
+    public ResponseEntity<String> version() {
+        return ResponseEntity.ok("0.0.3");
     }
 }
