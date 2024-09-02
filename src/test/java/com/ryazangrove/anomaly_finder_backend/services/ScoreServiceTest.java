3a1cb3da2e8975634aa0476340a364ba82d3b329
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-09-02 15:56:15 +0300

    add tests for getScores for ScoreService

---
+++ src/test/java/com/ryazangrove/anomaly_finder_backend/services/ScoreServiceTest.java
@@ -0,0 +1,54 @@
+package com.ryazangrove.anomaly_finder_backend.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ryazangrove.anomaly_finder_backend.models.Score;
+import com.ryazangrove.anomaly_finder_backend.repository.ScoreRepository;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ScoreServiceTest {
+    
+    @Mock
+    private ScoreRepository scoreRepository;
+
+    @InjectMocks
+    private ScoreService scoreService;
+
+    @Test
+    public void shouldReturnListOfScores() {
+        // Arrange
+        when(scoreRepository.findAll()).thenReturn(Arrays.asList(
+                new Score(101l, 21000l, "John Doe"),
+                new Score(102l, 5600l, "Jane Doe")
+        ));
+
+        // Act
+        List<Score> scores = scoreService.getScores();
+
+        // Assert
+        verify(scoreRepository, times(1)).findAll();
+        assertEquals(scores.size(), 2);
+        Score score0 = scores.get(0);
+        assertEquals(score0.getId(), 101l);
+        assertEquals(score0.getScore(), 21000l);
+        assertEquals(score0.getNickname(), "John Doe");
+        Score score1 = scores.get(1);
+        assertEquals(score1.getId(), 102l);
+        assertEquals(score1.getScore(), 5600l);
+        assertEquals(score1.getNickname(), "Jane Doe");
+    }
+}
