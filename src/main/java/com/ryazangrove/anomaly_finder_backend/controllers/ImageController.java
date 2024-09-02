46bea57d491297ae32cae4f8d0320fc9828ebd2b
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-09-02 20:53:07 +0300

    add REST endpoint documentation for ImageController

--- src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/ImageController.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/ImageController.java
@@ -11,6 +11,11 @@ import com.ryazangrove.anomaly_finder_backend.exceptions.ImageNotFoundException;
 import com.ryazangrove.anomaly_finder_backend.exceptions.NotImplementedException;
 import com.ryazangrove.anomaly_finder_backend.services.ImageInfoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,6 +26,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/images")
@@ -29,6 +36,15 @@ public class ImageController {
     @Autowired
     private ImageInfoService imageInfoService;
 
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Returns found image in the response body by provided image file name.",
+            content = @Content(mediaType = MediaType.IMAGE_JPEG_VALUE)),
+        @ApiResponse(responseCode = "404", description = "Could not find image or image info with provided id. Returns error message.",
+            content = @Content(examples = {
+                @ExampleObject(
+                    value = "{\"error\": \"Could not find metadata for image with id 10\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+    })
     @GetMapping("/{imageFileName}")
     public ResponseEntity<?> serveImageHandler(@PathVariable String imageFileName, HttpServletResponse response) throws IOException {
         try { 
@@ -37,7 +53,9 @@ public class ImageController {
             StreamUtils.copy(image,response.getOutputStream());  
             return new ResponseEntity(HttpStatus.OK);
         } catch (ImageInfoNotFoundException | ImageNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+            Map<String, String> responseMap = new HashMap<>();
+            responseMap.put("error", ex.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMap);
         }
     }
     
