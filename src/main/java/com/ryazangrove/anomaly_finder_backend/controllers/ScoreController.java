8c662843a89cc1b1302c16fc8c5b00d95880b029
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-09-02 20:13:24 +0300

    add REST endpoint documentation for ScoreController

--- src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/ScoreController.java
+++ src/main/java/com/ryazangrove/anomaly_finder_backend/controllers/ScoreController.java
@@ -8,13 +8,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import com.ryazangrove.anomaly_finder_backend.exceptions.ScoreNotFoundException;
 import com.ryazangrove.anomaly_finder_backend.models.Score;
 import com.ryazangrove.anomaly_finder_backend.services.ScoreService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/scores")
@@ -23,23 +31,54 @@ public class ScoreController {
     @Autowired
     private ScoreService scoreService;
 
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Returns list of all scores.",
+            content = @Content(examples = {
+                @ExampleObject(
+                    value = "[{\"id\": 1, \"score\": 10000, \"nickname\": \"John Doe\"}," + //
+                    "{\"id\": 2, \"score\": 195000, \"nickname\": \"Jane Doe\"}]")
+                }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+    })
     @GetMapping
     public ResponseEntity<List<Score>> get() {
         return ResponseEntity.ok(scoreService.getScores());
     }
 
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Returns empty JSON object.",
+            content = @Content(examples = {
+                @ExampleObject(
+                    value = "{}")
+                }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+    })
     @PostMapping
-    public ResponseEntity<Void> post(@RequestBody Score score) {
+    public ResponseEntity<String> post(@RequestBody Score score) {
         scoreService.saveScore(score);
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok("{}");
     }
 
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Returns score object with provided id.",
+            content = @Content(examples = {
+                @ExampleObject(
+                    value = "{\"id\": 1, \"score\": 10001, \"nickname\": \"John Doe\"}")
+                }, mediaType = MediaType.APPLICATION_JSON_VALUE)
+        ),
+        @ApiResponse(responseCode = "404", description = "Could not find score with provided id. Returns error message.",
+            content = @Content(examples = {
+                @ExampleObject(
+                    value = "{\"error\": \"Could not find score with id 10\"}")
+                }, mediaType = MediaType.APPLICATION_JSON_VALUE)
+        )
+    })
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(scoreService.getScore(id));
         } catch (ScoreNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+            Map<String, String> responseMap = new HashMap<>();
+            responseMap.put("error", ex.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMap);
         }
     }
 }
